4d1e8c46ee9b62fb83f19c99b2d1bf0944999642
debapriya.basu1@ingka.ikea.com <98165961+Debapriya-tech@users.noreply.github.com>
2023-08-11 08:12:21 +0200

    Creating Django APP with hello world view

--- chefsTable/demoApp/apps.py
+++ chefsTable/demoApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TestdemoappConfig(AppConfig):
+class DemoappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'testDemoAPP'
+    name = 'demoApp'
