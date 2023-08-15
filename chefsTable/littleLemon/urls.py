93c97188a0c6b4cbc5f72dca39e1ac96e680e85c
debapriya.basu1@ingka.ikea.com <98165961+Debapriya-tech@users.noreply.github.com>
2023-08-15 06:29:31 +0200

    Add App in the project

---
+++ chefsTable/littleLemon/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.hello, ),
+    path('menu/<int:menu_id>', views.menu_by_id, name = 'menu_by_id'),
+]
