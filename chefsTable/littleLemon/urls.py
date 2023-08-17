a4c40fcd36187dfa8ee34ae56a960ff09b62b571
debapriya.basu1@ingka.ikea.com <98165961+Debapriya-tech@users.noreply.github.com>
2023-08-17 07:35:39 +0200

    Update views and url patterns

--- chefsTable/littleLemon/urls.py
+++ chefsTable/littleLemon/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello, ),
+    path('', views.say_hello, ),
     path('menu/<int:menu_id>', views.menu_by_id, name = 'menu_by_id'),
 ]
