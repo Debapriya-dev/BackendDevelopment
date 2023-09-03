96c985ae0dec861f083c177963a23c1757100f95
debapriya.basu1@ingka.ikea.com <98165961+Debapriya-tech@users.noreply.github.com>
2023-09-03 08:44:32 +0200

    REquest paratm addition

--- chefsTable/littleLemon/views.py
+++ chefsTable/littleLemon/views.py
@@ -20,3 +20,23 @@ def menu_by_id(request, menu_id):
     menu = Menu.objects.get(pk=menu_id)
     #return HttpResponse(f"{menu.menu_item}: Type of {menu.cuisine} cuisine.")
     return render(request, 'menu_card.html', {'menu': menu})
+
+#Path parameter in URL
+def pathview(request, name, id):
+    return HttpResponse("Name : {} UserID : {}".format(name, id))
+
+#Query parameter in URL
+def queryview(request):
+    queryName = request.GET['name']
+    queryID = request.GET['id']
+    return HttpResponse("Name : {} UserID : {}".format(queryName, queryID))
+
+#Body parameter in URL
+def showform(request):
+    return render(request, "form.html")
+
+def getform(request):
+    if request.method == "POST":
+        bodyID = request.POST['id']
+        bodyName = request.POST['name']
+    return HttpResponse("Name : {}  UserID: {}".format(bodyName, bodyID))
